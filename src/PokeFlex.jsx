3e00c48456d3eaf8128c92db302ce9dd39f960ed
JotaVitorSilva <rm93927@estudante.fieb.edu.br>
2025-05-23 11:29:59 +0000

    grid

---
+++ src/PokeFlex.jsx
@@ -0,0 +1,26 @@
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './PokeFlex.css'
+const PokeFlex = () =>
+{
+    return (
+        <div classname="PokeFlex">
+           <img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
+
+<img src={poke}/>
+            <img src={eevee}/>
+
+<img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
+
+
+        </div>
+    )
+}
+
+export default PokeFlex
